0a7eda190bb794ea93d603e07f3a937e6923ae64
PhiloCwh <105807963+PhiloCwh@users.noreply.github.com>
2024-11-15 18:23:20 +0800

    Update stakeToken.sol

--- contracts/stakeToken.sol
+++ contracts/stakeToken.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ST is ERC20, Ownable {
+
+
+
+
+
+
+    constructor() ERC20("StakeToken", "ST") 
+    Ownable(msg.sender)
+    {
+        
+    }
+
+    function mint(address account,uint256 amount) public onlyOwner{
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner
+    {
+        _burn(account,amount);
+    }
+
+
+}
